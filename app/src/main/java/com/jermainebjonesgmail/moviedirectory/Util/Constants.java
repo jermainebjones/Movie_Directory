6c7f600eb9c3825e7b9aafda671b2b934dc28595
jermainebjones <39226126+jermainebjones@users.noreply.github.com>
2020-05-10 16:28:54 -0400

    Initial commit

---
+++ app/src/main/java/com/jermainebjonesgmail/moviedirectory/Util/Constants.java
@@ -0,0 +1,9 @@
+package com.jermainebjonesgmail.moviedirectory.Util;
+
+public class Constants {
+
+    public static final String URL_LEFT = "https://www.omdbapi.com/?s=";
+    public static final String URL = "https://www.omdbapi.com/?i=";
+    public static final String API_KEY = "&apikey=4f95e8dd";
+    public static final String URL_RIGHT = "&page=2&apikey=4f95e8dd";
+}
